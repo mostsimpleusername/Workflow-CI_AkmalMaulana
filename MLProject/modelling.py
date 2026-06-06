b2d0ecb19156fcb6b4f5e1039a46e7fd6701b354
mostsimpleusername <maulanaakmal484@gmail.com>
2026-06-06 14:03:02 +0700

    refactor: streamline MLflow integration and Conda setup in CI workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,8 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-mlflow.set_experiment("Telco_Customer_Churn")
-
 mlflow.sklearn.autolog()
 
 df = pd.read_csv("customer_churn_processed.csv")
@@ -23,27 +21,21 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-with mlflow.start_run():
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
 
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
-    )
+model.fit(
+    X_train,
+    y_train
+)
 
-    model.fit(
-        X_train,
-        y_train
-    )
+predictions = model.predict(X_test)
 
-    predictions = model.predict(
-        X_test
-    )
+accuracy = accuracy_score(
+    y_test,
+    predictions
+)
 
-    accuracy = accuracy_score(
-        y_test,
-        predictions
-    )
-
-    print(
-        f"Accuracy: {accuracy}"
-    )
+print(f"Accuracy: {accuracy}")
